6aa2e09c6ab95927d42a81aa4ec5ce8ff15a774d
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-06-22 16:52:04 +0200

    Creato RazzaRepo

---
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/repos/RazzaRepo.java
@@ -0,0 +1,9 @@
+package com.rifugio.rifugio.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rifugio.rifugio.entities.Razza;
+
+public interface RazzaRepo extends JpaRepository<Razza, Integer> {
+
+}
