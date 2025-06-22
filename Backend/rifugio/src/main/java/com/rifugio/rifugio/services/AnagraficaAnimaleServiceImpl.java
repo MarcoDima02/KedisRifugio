4366d07e91bf18a1b4677e5dbf4f9b90e0f06f0a
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-06-22 17:22:09 +0200

    Creato AnagraficaAnimaliSerrvice
    Aggiunto solo per il metodo per trovare tutti gli animali, per adesso

---
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/services/AnagraficaAnimaleServiceImpl.java
@@ -0,0 +1,20 @@
+package com.rifugio.rifugio.services;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.rifugio.rifugio.entities.AnagraficaAnimali;
+import com.rifugio.rifugio.repos.AnagraficaAnimaliRepo;
+
+@Service
+public class AnagraficaAnimaleServiceImpl implements AnagraficaAnimaliService {
+
+    private AnagraficaAnimaliRepo anagraficaAnimaliRepo;
+
+    public List<AnagraficaAnimali> getAllAnagraficaAnimali() {
+        return anagraficaAnimaliRepo.findAll();
+    }
+
+}
--- Backend/rifugio/src/main/java/com/rifugio/rifugio/services/EsempioService.java
+++
@@ -1,5 +0,0 @@
-package com.rifugio.rifugio.services;
-
-public interface EsempioService {
-
-}
