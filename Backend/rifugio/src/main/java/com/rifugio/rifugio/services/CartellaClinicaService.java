d855e7287c05d5da4f3f32e1fa29c531eb1a0388
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-06-22 17:48:40 +0200

    Creato SpecieService

---
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/services/CartellaClinicaService.java
@@ -0,0 +1,11 @@
+package com.rifugio.rifugio.services;
+
+import java.util.List;
+
+import com.rifugio.rifugio.entities.CartellaClinica;
+
+public interface CartellaClinicaService {
+
+    List<CartellaClinica> getAllCartelleCliniche();
+
+}
