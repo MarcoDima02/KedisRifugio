8e6ed2f478879d70b414e45aed4560202bc3214b
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-06-29 17:51:37 +0200

    FIX : corretto il richiamo dell'oggetto Anagrafica_Animali

--- Backend/rifugio/src/main/java/com/rifugio/rifugio/repos/ImmagineRepo.java
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/repos/ImmagineRepo.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rifugio.rifugio.entities.Immagine;
-import com.rifugio.rifugio.entities.Anagrafica_Animali;
+import com.rifugio.rifugio.entities.AnagraficaAnimali;;
 
 @Repository
 public interface ImmagineRepo extends JpaRepository<Immagine, Integer> {
-    List<Immagine> findByAnimale(Anagrafica_Animali animale);
+    List<Immagine> findByAnimale(AnagraficaAnimali animale);
 }
