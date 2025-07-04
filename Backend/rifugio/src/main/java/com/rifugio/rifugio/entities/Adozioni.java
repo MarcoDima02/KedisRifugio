504034750b634199b8b759c37926717e93101386
Lorenzo Maero <lorenzo.maero@edu-its.it>
2025-07-04 10:49:24 +0200

    Adozioni: fix entità

--- Backend/rifugio/src/main/java/com/rifugio/rifugio/entities/Adozioni.java
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/entities/Adozioni.java
@@ -2,10 +2,13 @@ package com.rifugio.rifugio.entities;
 
 import java.util.Date;
 
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Adozioni {
@@ -14,13 +17,19 @@ public class Adozioni {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id_adozione;
 
-    private Integer id_animale;
+    @ManyToOne
+    @JoinColumn(name = "id_animale", referencedColumnName = "id_animale")
+    private AnagraficaAnimali id_animale;
 
-    private Integer id_persona;
+    @ManyToOne
+    @JoinColumn(name = "id_persona", referencedColumnName = "id_persona")
+    private Utenti id_persona;
 
     private Date data_adozione;
 
-    private Integer id_step_adozioni;
+    @OneToMany
+    @JoinColumn(name = "id_step_adozioni", referencedColumnName = "id_step_adozioni")
+    private StepAdozioni id_step_adozioni;
 
 
     public Integer getId_adozione() {
@@ -31,19 +40,19 @@ public class Adozioni {
         this.id_adozione = id_adozione;
     }
 
-    public Integer getId_animale() {
+    public AnagraficaAnimali getId_animale() {
         return id_animale;
     }
 
-    public void setId_animale(Integer id_animale) {
+    public void setId_animale(AnagraficaAnimali id_animale) {
         this.id_animale = id_animale;
     }
 
-    public Integer getId_persona() {
+    public Utenti getId_persona() {
         return id_persona;
     }
 
-    public void setId_persona(Integer id_persona) {
+    public void setId_persona(Utenti id_persona) {
         this.id_persona = id_persona;
     }
 
@@ -55,11 +64,11 @@ public class Adozioni {
         this.data_adozione = data_adozione;
     }
 
-    public Integer getId_step_adozioni() {
+    public StepAdozioni getId_step_adozioni() {
         return id_step_adozioni;
     }
 
-    public void setId_step_adozioni(Integer id_step_adozioni) {
+    public void setId_step_adozioni(StepAdozioni id_step_adozioni) {
         this.id_step_adozioni = id_step_adozioni;
     }
 
