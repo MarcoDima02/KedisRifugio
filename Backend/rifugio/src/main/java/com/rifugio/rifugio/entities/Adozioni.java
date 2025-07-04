d708a952c6afcc7f3b0ab06b7b13f9ee661c60f9
Lorenzo Maero <lorenzo.maero@edu-its.it>
2025-07-04 13:12:31 +0200

    Adozioni: Creazione CRUD

--- Backend/rifugio/src/main/java/com/rifugio/rifugio/entities/Adozioni.java
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/entities/Adozioni.java
@@ -1,76 +1,70 @@
 package com.rifugio.rifugio.entities;
 
+import jakarta.persistence.*;
 import java.util.Date;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-
 @Entity
+@Table(name = "adozioni")
 public class Adozioni {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id_adozione;
+    @Column(name = "IdAdozione")
+    private Integer id;
 
     @ManyToOne
     @JoinColumn(name = "id_animale", referencedColumnName = "id_animale")
-    private AnagraficaAnimali id_animale;
+    private AnagraficaAnimali animale;
 
     @ManyToOne
     @JoinColumn(name = "id_persona", referencedColumnName = "id_persona")
-    private Utenti id_persona;
+    private Utenti persona;
 
-    private Date data_adozione;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_adozione")
+    private Date dataAdozione;
 
-    @OneToMany
+    @ManyToOne
     @JoinColumn(name = "id_step_adozioni", referencedColumnName = "id_step_adozioni")
-    private StepAdozioni id_step_adozioni;
+    private StepAdozioni stepAdozione;
 
-
-    public Integer getId_adozione() {
-        return id_adozione;
+    public Integer getId() {
+        return id;
     }
 
-    public void setId_adozione(Integer id_adozione) {
-        this.id_adozione = id_adozione;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public AnagraficaAnimali getId_animale() {
-        return id_animale;
+    public AnagraficaAnimali getAnimale() {
+        return animale;
     }
 
-    public void setId_animale(AnagraficaAnimali id_animale) {
-        this.id_animale = id_animale;
+    public void setAnimale(AnagraficaAnimali animale) {
+        this.animale = animale;
     }
 
-    public Utenti getId_persona() {
-        return id_persona;
+    public Utenti getPersona() {
+        return persona;
     }
 
-    public void setId_persona(Utenti id_persona) {
-        this.id_persona = id_persona;
+    public void setPersona(Utenti persona) {
+        this.persona = persona;
     }
 
-    public Date getData_adozione() {
-        return data_adozione;
+    public Date getDataAdozione() {
+        return dataAdozione;
     }
 
-    public void setData_adozione(Date data_adozione) {
-        this.data_adozione = data_adozione;
+    public void setDataAdozione(Date dataAdozione) {
+        this.dataAdozione = dataAdozione;
     }
 
-    public StepAdozioni getId_step_adozioni() {
-        return id_step_adozioni;
+    public StepAdozioni getStepAdozione() {
+        return stepAdozione;
     }
 
-    public void setId_step_adozioni(StepAdozioni id_step_adozioni) {
-        this.id_step_adozioni = id_step_adozioni;
+    public void setStepAdozione(StepAdozioni stepAdozione) {
+        this.stepAdozione = stepAdozione;
     }
-
-
 }
