66bc3986c38f19a15913aa26da55abbbf94c81f0
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-07-11 06:37:36 +0200

    aggiunto il mapping per la pagina donazioni

--- Backend/rifugio/src/main/java/com/rifugio/rifugio/controller/DashboardUtentiController.java
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/controller/DashboardUtentiController.java
@@ -1,5 +1,5 @@
 package com.rifugio.rifugio.controller;
 
 public class DashboardUtentiController {
-    
+
 }
--- Backend/rifugio/src/main/java/com/rifugio/rifugio/controller/RifugioController.java
+++ Backend/rifugio/src/main/java/com/rifugio/rifugio/controller/RifugioController.java
@@ -11,12 +11,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import jakarta.servlet.http.HttpSession;
 
@@ -83,5 +79,10 @@ public class RifugioController {
         return "lista_animali";
     }
 
+    @GetMapping("/donazioni")
+    public String sezioneComeAiutare() {
+        return "donazioni";
+    }
+    
 
 }
